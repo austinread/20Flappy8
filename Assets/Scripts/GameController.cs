cab69c51ff592c0b088873effcd5ba5a0f7a98cb
Austin <aread1800@gmail.com>
2015-04-07 21:34:48 -0400

    Pipes autogenerate and work correctly, now two objects, a top and bottom pipe prefab

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -3,8 +3,14 @@ using System.Collections;
 
 //Handles overarching game behavior:  Score, starting, losing, etc.
 public class GameController : MonoBehaviour {
-	public Vector2 gameSpeed;
+	public Vector2 gameSpeed;	//These are public for easy tweaking
 	public float birdGravity;
+	public float pipeInterval;	//How often they appear
+	public float pipeSpacing;	//In between the top and bottom pipes
+	public GameObject pipePrefabDown;
+	public GameObject pipePrefabUp;
+
+	private float timer;	//pipes
 
 	private GameObject[] ground;
 	private GameObject[] pipes;
@@ -14,6 +20,7 @@ public class GameController : MonoBehaviour {
 
 	void Start(){
 		paused = true;
+		timer = pipeInterval;
 		bird = GameObject.FindGameObjectWithTag("Bird");
 		ground = GameObject.FindGameObjectsWithTag("Ground");
 	}
@@ -25,13 +32,12 @@ public class GameController : MonoBehaviour {
 		foreach (GameObject block in ground){
 			block.GetComponent<ScrollingMovement>().Speed = gameSpeed;
 		}
-
-		//TODO: Pipe generation goes here
 	}
 
 	public void LoseGame(){
 		paused = true;
-		bird.GetComponent<Rigidbody2D>().gravityScale = (float)0.0;
+		timer = pipeInterval;
+		bird.GetComponent<Rigidbody2D>().gravityScale = 0.0f;
 		bird.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
 		bird.transform.position = bird.GetComponent<FlappyFlappin>().StartingPos;
 
@@ -46,11 +52,31 @@ public class GameController : MonoBehaviour {
 		}
 	}
 
+	//At globally defined interval, spawns a new set of pipes at a slightly random location
+	void SpawnPipes(){
+		timer -= Time.deltaTime;
+		if (timer <= 0){
+			timer = pipeInterval;
+
+			float randomOffset = Random.Range(-3.0f, 3.0f);
+			Vector2 newPipeLocation = new Vector2(8, -10 + randomOffset);	//BEWARE, HERE BE HARDCODED (X, Y) VALUES BY A LAZY ASS DEVELOPER
+			GameObject newPipeDown = (GameObject)Instantiate(pipePrefabDown, newPipeLocation, Quaternion.identity);
+			GameObject newPipeUp = (GameObject)Instantiate(pipePrefabUp, new Vector2(newPipeLocation.x, newPipeLocation.y + pipeSpacing), Quaternion.identity);
+			newPipeDown.GetComponent<ScrollingMovement>().Speed = gameSpeed;
+			newPipeUp.GetComponent<ScrollingMovement>().Speed = gameSpeed;
+		}
+	}
+
 	void Update(){
 		float inputX = Input.GetAxis ("Horizontal");
 		float inputY = Input.GetAxis ("Vertical");
 		if ((inputX != 0 || inputY != 0) && paused){
 			StartGame();
 		}
+
+		if (!paused){
+			SpawnPipes();
+		}
+
 	}
 }
